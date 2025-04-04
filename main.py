c7e15698b252d4376c9bc26f6c7aab0a26e36735
Areeb Tahir <135326733+Areeb331@users.noreply.github.com>
2025-04-04 23:18:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,5 @@ def generate_meal_plan():
         print("❌ Server Error:", str(e))
         return jsonify({'error': str(e)}), 500
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
