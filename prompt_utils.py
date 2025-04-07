576352e62d0d3ef002053a078d3631c91d2c8007
Areeb Tahir <135326733+Areeb331@users.noreply.github.com>
2025-04-08 00:09:10 +0500

    Update prompt_utils.py

--- prompt_utils.py
+++ prompt_utils.py
@@ -71,7 +71,7 @@ Meal Plan Rules:
   - “Here’s your meal plan” style intro
   - Anything outside Day {day_range.split("-")[0]} to Day {day_range.split("-")[1]}
   - Make sure each meal shows its nutrition like:
-    Calories: 550 kcal, Protein: 45g, Carbs: 55g, Fats: 12g 
+    Calories: 550 kcal, Protein: 45g, Carbs: 55g, Fats: 12g
     Do not change this format. Always use this exact structure after each meal.
 
 
